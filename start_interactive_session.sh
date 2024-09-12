7a88cb6693c6f0694f9930ab592c552055a52c60
VishGit1234 <47926842+VishGit1234@users.noreply.github.com>
2024-09-11 21:15:10 -0400

    remove added newline

--- start_interactive_session.sh
+++ start_interactive_session.sh
@@ -42,5 +42,4 @@ sed -i 's/\r$//' $REMOTE_SLURM_FILE
 srun --cpus-per-task=$NUMER_OF_CPUS --mem=$MEMORY \\
     --gres=tmpdisk:$TMP_DISK_SIZE,shard=$VRAM --time=$USAGE_TIME --job-name=wato_slurm_dev \\
     --pty bash ~/slurm_tooling/asd_interactive_job.slurm
-
 EOF
