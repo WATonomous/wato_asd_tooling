a2372b5de42d3dc5ea6b0ec5c2180be6e9fba0e8
Jimmy Jiang <j257jian@uwaterloo.ca>
2024-09-17 03:29:47 +0000

    WIP: initial commit

--- start_interactive_session.sh
+++ start_interactive_session.sh
@@ -43,3 +43,5 @@ srun --cpus-per-task=$NUMER_OF_CPUS --mem=$MEMORY \\
     --gres=tmpdisk:$TMP_DISK_SIZE,shard=$VRAM --time=$USAGE_TIME --job-name=wato_slurm_dev \\
     --pty bash ~/slurm_tooling/asd_interactive_job.slurm
 EOF
+
+ssh -t HOST ~/some_script.sh
