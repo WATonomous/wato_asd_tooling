1df7a173994b6c930f5e39be40ef71a218a0075b
Eddy Zhou <edward.zhzh@gmail.com>
2024-09-21 18:36:15 -0400

    add update wato asd tooling

--- session_config.sh
+++ session_config.sh
@@ -9,8 +9,11 @@ export REMOTE_HOST="watcloud_remote_host"      # [tr-ubuntu3, derek3-ubuntu2]
 export SSH_KEY="path_to_private_key"           # Path to your local private key
 
 # SLURM job configuration
-export NUMBER_OF_CPUS=4         # Number of CPUs to use
+export NUMBER_OF_CPUS=4        # Number of CPUs to use
 export MEMORY=16G              # Amount of RAM to use
 export USAGE_TIME="6:00:00"    # How long you want to run the session for
 export TMP_DISK_SIZE=10240     # How much temporary storage you want [in MiB]
 export VRAM=0                  # How much GPU VRAM you want [in MiB]
+
+# SLURM tooling configuration
+export UPDATE_WATO_ASD_TOOLING=0 # Set to 1 if you've recently pulled this repo
